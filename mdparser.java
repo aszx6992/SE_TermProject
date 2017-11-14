038afb8ff2a83376d9a6da5f238ff6a52369473a
root <root@ip-172-31-9-86.ap-northeast-2.compute.internal>
2017-11-14 13:41:58 +0000

    "Add MDConvSystem"

---
+++ mdparser.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class mdparser {
+	public static void main (String args[]){
+		LinkedList<String> options = new LinkedList<String>();
+		LinkedList<String> mdFiles = new LinkedList<String>();
+		LinkedList<String> htmlName = new LinkedList<String>();
+
+		String[] commands = {"-pl", "-st", "-sl","-h", "--help"};
+
+		for(int i=0; i < args.length; i++){
+			if(args[i].charAt(0) == '-') {
+				if(args[i].equals("-h") || args[i].equals("--help")) {
+					System.out.println("This is the help message");
+					System.exit(0);
+				}
+				options.add(args[i]);
+			}
+			else if(args[i].substring(args[i].length()-2).equals("md")) {
+				mdFiles.add(args[i]);
+
+				if(i + 1 < args.length && args[i+1].substring(args[i+1].length()-4).equals("html")){
+					htmlName.add(args[i+1]);
+					i++;
+				}
+				else {
+					htmlName.add(args[i].substring(0, args[i].length() - 2) + "html");
+				}
+			}
+			else {
+				System.out.println("Please input a file extention of '.md' for conversion");
+				System.exit(0);
+			}
+		}
+		
+		for(int i=0; i < options.size(); i++){
+			Boolean flag = false;
+			for(int j=0; j < commands.length; j++){
+				if(options.get(i).equals(commands[j])) {
+					flag = true;
+					//insert option action here
+				}
+			}
+			if(!flag) {
+				System.out.println("mdparser: invalid option -- " + "'" + options.get(i) + "'");
+				System.out.println("Try 'mdparser --help' for more information");
+				System.exit(0);
+			}
+		}
+
+		for(int i=0; i < options.size(); i++){
+			System.out.println(options.get(i));
+		}
+
+		for(int i=0; i < mdFiles.size(); i++){
+ 	        	System.out.print(mdFiles.get(i));
+			System.out.print("  ");
+         		System.out.println(htmlName.get(i));
+		}
+       	}
+
+}
+
+		
