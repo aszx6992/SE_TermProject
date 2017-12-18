679538c40b475568e25d6384d58c89eeb4eb3f7e
dmdwns67 <dmdwns67@naver.com>
2017-12-18 20:34:15 +0900

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Converter {
 	private String buffer, splice_buffer, instring_buffer, result;
 	private StringBuffer return_write;
-	boolean doNeedInString;
+	boolean doNeedInString, IsPrefixTest;
 	
 	Converter(){
 		buffer = null;
@@ -11,6 +11,7 @@ public class Converter {
 		instring_buffer = null;
 		result = null;
 		doNeedInString = true;
+		IsPrefixTest = false;
 		return_write = new StringBuffer();
 	}
 	
@@ -18,6 +19,8 @@ public class Converter {
 		this.splice_buffer = null;
 		this.instring_buffer = null;
 		this.result = null;
+		this.doNeedInString = true;
+		this.IsPrefixTest = false;
 	}
 	
 	public String run(BufferedReader inputReader){
@@ -52,60 +55,75 @@ public class Converter {
 			    		System.out.println("Here is in line Detect");
 			    		
 			    		doNeedInString = false;
-					
+			    		IsPrefixTest = true;
+			    		
 			    		result = detectLine.transformToHTML();
 			    	}
 			    	// Title Check
-			    	else if(detectTitle.isTitle(buffer)){
+			    	else if(detectTitle.isTitle(buffer) && !IsPrefixTest){
 			    		// here is in Title Case and do not need to do InString Test
 			    		System.out.println("Here is in Title Detect");
 					
 			    		doNeedInString = false;
+			    		IsPrefixTest = true;
 					
 			    		result = detectTitle.transformToHTML(buffer, detectTitle.getSharpNum());
 			    	}
 			    	// BLOCK TEST -> instring test를 거쳐야 되는 거고.
-			    	// List Check
-			    	else if(detectList.isList(buffer)){
-			    		// here is in List Case and need to do InString Test
-			    		System.out.println("Here is in list Detect");
+			    	else if(!IsPrefixTest){ 
+			    		// List Check
+			    		if(detectList.isList(buffer)){
+			    			// here is in List Case and need to do InString Test
+			    			System.out.println("Here is in list Detect");
 					
-			    		doNeedInString = true;
+			    			doNeedInString = true;
 					
-			    		// splice buffer and send it to InString Test
-			    		splice_buffer = detectList.spliceBuffer(buffer);
+			    			// splice buffer and send it to InString Test
+			    			splice_buffer = detectList.spliceBuffer(buffer);
 					
-			    		System.out.println("splice_buffer : " + splice_buffer);
+			    			System.out.println("splice_buffer : " + splice_buffer);
+			    		}
+				
+				
+			    		// InString Test only when doNeedInString is true
+			    		if(doNeedInString == true){
+			    			// instring method is here
+			    			// instring_buffer = INSTRING_TEST(splice_buffer)
+					
+			    			// update result
+							// just string without MD syntax
+							if(splice_buffer != null && result == null)
+								instring_buffer = splice_buffer;
+							if(splice_buffer != null && result != null)
+								instring_buffer = splice_buffer;
+							if(splice_buffer == null && result == null)
+								result = buffer;			
+			    		}
+			
+				
+			    		// Block Test has to wrap after InString Test.
+			    		// List Wrapping
+			    		if(detectList.isList(buffer))
+			    			result = detectList.transformToHTML(instring_buffer, detectList.getListOption());
+			    		
+			    		System.out.println("result :" + result);
+			    		System.out.println();
+				
+			    		// Append return_write
+			    		return_write.append(result + "\n");
+			
+			    		System.out.println("End Roop");
+				
+			    	}else{
+			    		System.out.println("result :" + result);
+			    		System.out.println();
+				
+			    		// Append return_write
+			    		return_write.append(result + "\n");
+			
+			    		System.out.println("End Roop");
 			    	}
-				
-				
-			    	// InString Test only when doNeedInString is true
-			    	if(doNeedInString == true){
-			    		// instring method is here
-			    		// instring_buffer = INSTRING_TEST(splice_buffer)
-					
-			    		// update result
-			    		// just string without MD syntax
-			    		if(splice_buffer != null && result == null)
-			    			result = splice_buffer;
-			    		if(splice_buffer == null && result == null)
-			    			result = buffer;			
-			    	}
-			
-				
-			    	// Block Test has to wrap after InString Test.
-			    	// List Wrapping
-			    	if(detectList.isList(buffer))
-			    		result = detectList.transformToHTML(instring_buffer, detectList.getListOption());
-				
-			
-			    	System.out.println("result :" + result);
-			    	System.out.println();
-				
-			    	// Append return_write
-			    	return_write.append(result + "\n");
-			
-			    	System.out.println("End Roop");
+			    
 			    } // if condition
 			} // while
 			
