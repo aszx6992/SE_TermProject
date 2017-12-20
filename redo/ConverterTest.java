79644cc0493388656842911111688b6236c1d870
dmdwns67 <dmdwns67@naver.com>
2017-12-20 20:24:19 +0900

    Add files via upload

--- redo/ConverterTest.java
+++ redo/ConverterTest.java
@@ -22,37 +22,37 @@ public class ConverterTest {
 		// test4 : lists
 		// normal + normal
 		String[] tc4_1 ={"* Markdown supports ordered (numbered) and unordered (bulleted) lists."
-				,"* It¡¯s important to note that the actual numbers you use to mark the list have"
+				,"* Itâ€™s important to note that the actual numbers you use to mark the list have"
 		};
 		converter.init();
 		assertEquals("<ul>\n"
 				+ "<li>Markdown supports ordered (numbered) and unordered (bulleted) lists.</li>\n"
-				+ "<li>It¡¯s important to note that the actual numbers you use to mark the list have</li>\n"
+				+ "<li>Itâ€™s important to note that the actual numbers you use to mark the list have</li>\n"
 				+ "</ul>\n\n",converter.convert(tc4_1));
 		
 		// normal + nested
 		String[] tc4_2 ={"* Markdown supports ordered (numbered) and unordered (bulleted) lists."
-				,"  * It¡¯s important to note that the actual numbers you use to mark the list have"
+				,"  * Itâ€™s important to note that the actual numbers you use to mark the list have"
 		};
 		converter.init();
 		assertEquals("<ul>\n"
 				+ "<li>Markdown supports ordered (numbered) and unordered (bulleted) lists.\n"
 				+ "<ul>\n"
-				+ "<li>It¡¯s important to note that the actual numbers you use to mark the list have</li>\n"
+				+ "<li>Itâ€™s important to note that the actual numbers you use to mark the list have</li>\n"
 				+ "</ul>\n"
 				+ "</li>\n"
 				+ "</ul>\n\n", converter.convert(tc4_2));
 		
 		// normal + nested + normal
 		String[] tc4_3 = {"* Markdown supports ordered (numbered) and unordered (bulleted) lists."
-				,"  * It¡¯s important to note that the actual numbers you use to mark the list have"
+				,"  * Itâ€™s important to note that the actual numbers you use to mark the list have"
 				,"* If you instead wrote the list in Markdown like this"
 		};
 		converter.init();
 		assertEquals("<ul>\n"
 				+"<li>Markdown supports ordered (numbered) and unordered (bulleted) lists.\n"
 				+"<ul>\n"
-				+"<li>It¡¯s important to note that the actual numbers you use to mark the list have</li>\n"
+				+"<li>Itâ€™s important to note that the actual numbers you use to mark the list have</li>\n"
 				+"</ul>\n"
 				+"</li>\n"
 				+"<li>If you instead wrote the list in Markdown like this</li>\n"
@@ -60,19 +60,19 @@ public class ConverterTest {
 		
 		// normal + indent + normal
 		String[] tc4_4 ={"* Markdown supports ordered (numbered) and unordered (bulleted) lists."
-				,"  It¡¯s important to note that the actual numbers you use to mark the list have"
+				,"  Itâ€™s important to note that the actual numbers you use to mark the list have"
 				,"* If you instead wrote the list in Markdown like this"
 		};
 		converter.init();
 		assertEquals("<ul>\n" 
 				+"<li>Markdown supports ordered (numbered) and unordered (bulleted) lists.\n"
-				+"It¡¯s important to note that the actual numbers you use to mark the list have</li>\n"
+				+"Itâ€™s important to note that the actual numbers you use to mark the list have</li>\n"
 				+"<li>If you instead wrote the list in Markdown like this</li>\n"
 				+"</ul>\n\n", converter.convert(tc4_4));
 		
 		// normal + nested + indent + normal
 		String[] tc4_5 = {"* Markdown supports ordered (numbered) and unordered (bulleted) lists."
-				,"* It¡¯s important to note that the actual numbers you use to mark the list have"
+				,"* Itâ€™s important to note that the actual numbers you use to mark the list have"
 				,"  no effect on the HTML output Markdown produces. The HTML Markdown produces"
 				,"  from the above list is:"
 				,"* If you instead wrote the list in Markdown like this"
@@ -80,7 +80,7 @@ public class ConverterTest {
 		converter.init();
 		assertEquals("<ul>" + "\n" +
 					"<li>Markdown supports ordered (numbered) and unordered (bulleted) lists.</li>" + "\n" +
-					"<li>It¡¯s important to note that the actual numbers you use to mark the list have" + "\n" + 
+					"<li>Itâ€™s important to note that the actual numbers you use to mark the list have" + "\n" + 
 					"no effect on the HTML output Markdown produces. The HTML Markdown produces" + "\n" +
 					"from the above list is:</li>" + "\n" +
 					"<li>If you instead wrote the list in Markdown like this</li>" + "\n" +
@@ -88,7 +88,8 @@ public class ConverterTest {
 					
 		
 		// test5 : block quote
-		String[] tc5 = {">Markdown is intended to be as easy-to-read and easy-to-write as is feasible."
+		// '> at the first time and no space after > mark' case 
+		String[] tc5_1 = {">Markdown is intended to be as easy-to-read and easy-to-write as is feasible."
 				,"Readability, however, is emphasized above all else. A Markdown-formatted document should be publishable"
 				,""
 		};
@@ -96,7 +97,18 @@ public class ConverterTest {
 		assertEquals("<blockquote>\n"
 				+"  <p>Markdown is intended to be as easy-to-read and easy-to-write as is feasible.\n"
 				+"  Readability, however, is emphasized above all else. A Markdown-formatted document should be publishable</p>\n"
-				+"</blockquote>\n",converter.convert(tc5));
+				+"</blockquote>\n",converter.convert(tc5_1));
+		
+		// '> at every line and space after > mark' case
+		String[] tc5_2 ={"> Textile, reStructuredText, Grutatext, and EtText â€“ the single biggest source of inspiration for Markdownâ€™s"
+				,"> syntax is the format of plain text email."
+				,""
+		};
+		converter.init();
+		assertEquals("<blockquote>\n"
+				+"  <p>Textile, reStructuredText, Grutatext, and EtText â€“ the single biggest source of inspiration for Markdownâ€™s\n"
+				+"  syntax is the format of plain text email.</p>\n"
+				+"</blockquote>\n",converter.convert(tc5_2));
 		
 		// test 6 : Table
 		String[] tc6 = {"<table> <tr> <td> Deaedline </td> <td> 23:59, 21 Dec, 2017 </td> </tr>",
@@ -140,16 +152,48 @@ public class ConverterTest {
 		assertEquals("<h2>Ordered list</h2>\n",converter.convert(tc10));
 	
 		// test 11 : Edit url
-		String[] tc11 = {"Submit the report via [Hisnet](http://hisnet.handong.edu \"Hisnet\")."};
+		// '[](url)' case
+		String[] tc11_1 = {"Submit the report via [Hisnet](http://hisnet.handong.edu \"Hisnet\")."};
 		converter.init();
-		assertEquals("<p>Submit the report via <a href=\"http://hisnet.handong.edu\" title=\"Hisnet\">Hisnet</a>.</p>\n",converter.convert(tc11));
+		assertEquals("<p>Submit the report via <a href=\"http://hisnet.handong.edu\" title=\"Hisnet\">Hisnet</a>.</p>\n",converter.convert(tc11_1));
+		
+		// '[][1] -> [1]: url' case
+		String[] tc11_2 ={"The code of your project should be maintained in [GitHub][1]."
+				, "Do not forget to git push once you make git commit."
+				,""
+				,"[1]: Github.com (GitHub)"
+				,""
+		};
+		converter.init();
+		assertEquals("<p>The code of your project should be maintained in <a href=\"Github.com\" title=\"GitHub\">GitHub</a>. Do not forget to git push once you make git commit.</p>"
+				+"\n\n",converter.convert(tc11_2));
+		
+		// image case
+		String[] tc11_3={"All copy rights reserved in Handong Global University  ![tag](https://www.handong.edu/site/handong/res/img/logo.png)"
+				,""};
+		converter.init();
+		assertEquals("<p>All copy rights reserved in Handong Global University  <img src=\"https://www.handong.edu/site/handong/res/img/logo.png\" alt=\"tag\"></p>\n",converter.convert(tc11_3));
 		
 		// test 12 : plain text
-		String[] tc12 ={"This assignment askes you to build a program that reads a *Markdown* file and converts it to a HTML file."
+		// with *emphasize*
+		String[] tc12_1 ={"This assignment askes you to build a program that reads a *Markdown* file and converts it to a HTML file."
 				,"You are given a documentation on the Markdown syntax which starts as follows:"
 				};
 		converter.init();
-		assertEquals("<p>This assignment askes you to build a program that reads a <em>Markdown</em> file and converts it to a HTML file. You are given a documentation on the Markdown syntax which starts as follows:</p>\n",converter.convert(tc12));
+		assertEquals("<p>This assignment askes you to build a program that reads a <em>Markdown</em> file and converts it to a HTML file. You are given a documentation on the Markdown syntax which starts as follows:</p>\n",converter.convert(tc12_1));
+		
+		// with \<, \>
+		String[] tc12_2={"Markdown wraps a code block in both \\<pre\\> and \\<code\\> tags. For example:"};
+		converter.init();
+		assertEquals("<p>Markdown wraps a code block in both \\<pre\\> and \\<code\\> tags. For example:</p>\n",converter.convert(tc12_2));
+		
+		// pure plain text
+		String[] tc12_3={"Pre-formatted code blocks are used for writing about programming or markup source code."
+				,"Rather than forming normal paragraphs, the lines of a code block are interpreted literally."
+				,""
+		};
+		converter.init();
+		assertEquals("<p>Pre-formatted code blocks are used for writing about programming or markup source code. Rather than forming normal paragraphs, the lines of a code block are interpreted literally.</p>\n",converter.convert(tc12_3));
 		
 	}
 }
