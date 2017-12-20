821820a1d976d95b9c334a689897c3bfec691be4
Kyungjin Bae <leonardobae@naver.com>
2017-12-21 04:11:17 +0900

    modified mdparser(print statements)

--- src/mdparser.java
+++ src/mdparser.java
@@ -1,3 +1,5 @@
+package SE_package;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -15,11 +17,14 @@ public class mdparser {
 
     static String[] commands = {"-pl", "-st", "-sl"};
 
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
+    	//options.add("-pl");
         mdparser mdparser_instance = new mdparser();
         List<String> readBuffer = new ArrayList<String>();           //array list to save file
         String[] file;                                              //final string array to convert
         String line;                                                //read line by line
+
 
         BufferedReader reader = null;
         BufferedWriter writer = null;
@@ -27,53 +32,68 @@ public class mdparser {
 
         Converter converter;
 
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < args.length; i++) 
+        {
             if (args[i].charAt(0) == '-') {
-                if (args[i].equals("-h") || args[i].equals("--help")) {
+                if (args[i].equals("-h") || args[i].equals("--help")) 
+                {
                     System.out.print("This is the help message");
                     return;
                 }
-				else {
+				else					
+				{
                     //if valid option, add to command list
                     if(mdparser_instance.isValidOption(args[i]))
-                	    options.add(args[i]);
+                    {
+                	    options.add(args[i]);                	 
+                    }
                     //check if the entered option is valid
-                    else {
-                        System.out.println("mdparser: invalid option -- " + "'" + options + "'");
-                        System.out.println("Try 'mdparser --help' for more information");
+                    else 
+                    {
+                    	System.out.print("mdparser: invalid option.. Try 'mdparser --help' for more information");
                         return;
                     }
 
 				}
-            } else if (args[i].substring(args[i].length() - 2).equals("md")) {
+            } 
+            else if (args[i].substring(args[i].length() - 2).equals("md")) 
+            {
                 mdFiles.add(args[i]);
 
-                if (i + 1 < args.length && args[i + 1].substring(args[i + 1].length() - 4).equals("html")) {
+                if (i + 1 < args.length && args[i + 1].substring(args[i + 1].length() - 4).equals("html"))
+                {
                     htmlName.add(args[i + 1]);
                     i++;
-                } else {
-                    htmlName.add(args[i].substring(0, args[i].length() - 2) + "html");
                 }
-            } else {
-                System.out.println("Please input a file extention of '.md' for conversion");
+                else
+                {
+                		htmlName.add(args[i].substring(0, args[i].length() - 2) + "html");
+                }
+            }
+            else
+            {
+                System.out.print("Please input a file extention of '.md' for conversion");
 		        return;
             }
         }
 
         //Read file and convert
-        for (int i = 0; i < mdFiles.size(); i++) {
-            try {
+        for (int i = 0; i < mdFiles.size(); i++)
+        {
+            try
+            {
                 File directory = new File(mdFiles.get(i));
                 reader = new BufferedReader(new FileReader(directory));
 
                 //read file and save to arraylist buffer
-                while ((line = reader.readLine()) != null) {
+                while ((line = reader.readLine()) != null)
+                {
                     readBuffer.add(line);
                 }
                 file = readBuffer.toArray(new String[0]);               //convert arraylist to array of strings
 
 
-                writer = new BufferedWriter(new FileWriter("../doc/" + htmlName.get(i)));
+                writer = new BufferedWriter(new FileWriter(htmlName.get(i)));
                 converter = new Converter();
                 result = converter.convert(file);
 
@@ -82,28 +102,40 @@ public class mdparser {
 
                 System.out.println("Created new file(s) at \'../doc/\'!");
 
-            } catch (IOException e) {
+            } 
+            catch (IOException e) 
+            {
                 System.out.println("File cannot be found!");
                 e.printStackTrace();
-            } finally {
-                try {
-                    if (reader != null) {
+            } 
+            finally 
+            {
+                try 
+                {
+                    if (reader != null)
+                    {
                         reader.close();
                     }
-                    if (writer != null) {
+                    if (writer != null) 
+                    {
                         writer.close();
                     }
-                } catch (IOException ex) {
+                } 
+                catch (IOException ex) 
+                {
                     ex.printStackTrace();
                 }
             }
         }
     }
 
-    public boolean isValidOption(String option) {
+    public boolean isValidOption(String option)
+    {
         Boolean flag = false;
-        for (int j = 0; j < commands.length; j++) {
-            if (options.equals(commands[j])) {
+        for (int j = 0; j < commands.length; j++)
+        {
+            if (option.equals(commands[j]))
+            {
                 flag = true;
                 //insert option action here
             }
